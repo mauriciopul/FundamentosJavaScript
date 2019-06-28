4110c16568e4dd4f87a9103e4f2e7ccc1decf40c
Mauricio Pulgarin <mauriciopul@gmail.com>
2019-06-28 17:25:54 -0500

    Crear una funcion basica con y sin parametros

---
+++ clase4.js
@@ -0,0 +1,23 @@
+var nombre = 'Mauricio', edad = 35
+
+// //Funcion sin parámetros
+// function imprimirEdad(){
+//     console.log(`${nombre} tiene ${edad} años`)
+// }
+// imprimirEdad();
+
+//Función con parámetros
+function imprimirEdad(n,e){
+    console.log(`${n} tiene ${e} años`)
+}
+imprimirEdad(nombre, edad);
+imprimirEdad('Vicky', 24);
+imprimirEdad('Eric', 34);
+imprimirEdad('Dario', 76);
+
+
+
+
+
+
+
