ef832055d7fa471c68176a92a88379cb3517d7c1
Mauricio Pulgarin <mauriciopul@gmail.com>
2019-06-28 16:06:47 -0500

    manejo de caracteres

--- clase1.js
+++ clase1.js
@@ -1,8 +1,31 @@
-var nombre = 'Mao'
-var apellido = 'Pulgarin'
-edad = '35 años'
+var nombre = 'Mauricio', apellido = 'Pulgarin' //Se declaran variables con la palabra 'var'
+nombreCompleto = nombre + ' ' + apellido//Tambien se declaran variables sin la palabra 'var'NO ES RECOMENDABLE
+console.log(nombreCompleto)
+
+var nombreEnMayusculas = nombre.toUpperCase();//Se convierten todos los string en Mayúscula
+var apellidoEnMinusculas = apellido.toLowerCase();//Se convierten todos los string en Minúscula
+
+console.log('Mi nombre es ' + nombreEnMayusculas)
+console.log('Mi apellido es ' + apellidoEnMinusculas)
+
+var caracter = nombre.charAt(0);//Se asigna a la variable 'caracter' el caracter en la posición 0
+console.log(caracter)
+
+var cantLetrasDelNombre = nombre.length; //Cantidad de caracteres de un string
+console.log(cantLetrasDelNombre)
+
+//Concatenar con 'interpolacion de texto'
+nombreCompleto = `${nombre} ${apellido.toUpperCase()}   1`
+console.log(nombreCompleto)
+
+var prueba = `Esta es una prueba`
+console.log(prueba)
+
+//Opciones para escribir parte de un caracter
+var str1 = nombre.charAt(1)+nombre.charAt(2)
+console.log(str1)
+var str2 = nombre.substr (1,2)//el primer parametro indica donde inicia, el segundo dice cuantos caracteres muestra
+console.log(str2)
 
 
-console.log('hola '+nombre + ' '+apellido)
-console.log('Tengo '+edad)
 
