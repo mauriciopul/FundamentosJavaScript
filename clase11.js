2f82670e15fed7f5fec61583d77fbe6ed5c5945a
Mauricio Pulgarin <mauriciopul@gmail.com>
2019-07-02 09:17:10 -0500

    funciones con condicionales

---
+++ clase11.js
@@ -0,0 +1,37 @@
+var mao = {
+    nombre: 'Mauricio',
+    apellido: 'Pulgarin',
+    edad: 35,
+    ingeniero: true,
+    cocinero: false,
+    cantante: false,
+    dj: false,
+    guitarrista: false,
+    drone: true
+}
+var juli = {
+    nombre: 'Juliana',
+    apellido: 'Pulgarin Gomez',
+    edad: 14
+}
+
+const MAYORIA_DE_EDAD = 18
+
+function esMayorDeEdad(persona) {
+    return persona.edad >= MAYORIA_DE_EDAD
+}
+
+function imprimirSiEsMayorDeEdad(persona) {
+    if (esMayorDeEdad(persona)) {
+        console.log(`${persona.nombre} es Mayor de edad`)
+    } else {
+        console.log(`${persona.nombre} es Menor de edad`)
+    }
+}
+
+// imprimirSiEsMayorDeEdad(mao)
+
+
+
+
+
